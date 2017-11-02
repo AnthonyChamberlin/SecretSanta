18643c635d80897db5c0c0497662cba5634b2626
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-11-02 12:56:26 +0100

    Add User Profile Conditionals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.destroy_all
 
-jean = User.create!(email:"jean@secretsanta.com", password:"123456")
-martin = User.create!(email:"martin@secretsanta.com", password:"123456")
-joe = User.create!(email:"joe@secretsanta.com", password:"123456")
-ant = User.create!(email:"ant@secretsanta.com", password:"123456")
-ben = User.create!(email:"ben@secretsanta.com", password:"123456")
-stephen = User.create!(email:"stephen@secretsanta.com", password:"123456")
-phillip = User.create!(email:"phillip@secretsanta.com", password:"123456")
+martin = User.create!(first_name:"Martin", last_name:"Chamberlin", email:"martin@secretsanta.com", password:"123456")
+jean = User.create!(first_name:"Jean", last_name:"Chamberlin", email:"jean@secretsanta.com", password:"123456")
+joe = User.create!(first_name:"Joe", last_name:"Chamberlin", email:"joe@secretsanta.com", password:"123456")
+ant = User.create!(first_name:"Ant", last_name:"Chamberlin", email:"ant@secretsanta.com", password:"123456")
+ben = User.create!(first_name:"Ben", last_name:"Chamberlin", email:"ben@secretsanta.com", password:"123456")
+stephen = User.create!(first_name:"Stephen", last_name:"Chamberlin", email:"stephen@secretsanta.com", password:"123456")
+phillip = User.create!(first_name:"Pip", last_name:"Chamberlin", email:"phillip@secretsanta.com", password:"123456")
